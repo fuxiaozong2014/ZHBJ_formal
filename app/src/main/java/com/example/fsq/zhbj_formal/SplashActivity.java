c0d7551f8510623b477159a91b38017bb476f138
fushaoqing <fushaoqing2009@163.com>
2018-05-26 21:23:58 +0800

    Initial commit

---
+++ app/src/main/java/com/example/fsq/zhbj_formal/SplashActivity.java
@@ -0,0 +1,13 @@
+package com.example.fsq.zhbj_formal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
